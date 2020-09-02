6d718bfd2123220b5d6879b4be5eb1c725ab15ff
Jesus R. Gonzalez J <JGONZ@MacBook-Pro-JGONZ.local>
2020-09-02 16:59:16 -0400

    Updates

---
+++ precache-manifest.9da6dd09e3f9d4de968cb415fcf95243.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fdad6a4d03b66d6fffef37d3085c9e",
+    "url": "/github.io/caribbeanpay/index.html"
+  },
+  {
+    "revision": "4013df7dad1fb3e7c3c4",
+    "url": "/github.io/caribbeanpay/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "01f497f50b1b156fed4a",
+    "url": "/github.io/caribbeanpay/static/css/main.ab02bf86.chunk.css"
+  },
+  {
+    "revision": "4013df7dad1fb3e7c3c4",
+    "url": "/github.io/caribbeanpay/static/js/2.3d9bd26d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/caribbeanpay/static/js/2.3d9bd26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f497f50b1b156fed4a",
+    "url": "/github.io/caribbeanpay/static/js/main.28f78e4a.chunk.js"
+  },
+  {
+    "revision": "07d9561b09a20621b372",
+    "url": "/github.io/caribbeanpay/static/js/runtime-main.eabaa659.js"
+  },
+  {
+    "revision": "64b04226e021bba5b6aca00517df183d",
+    "url": "/github.io/caribbeanpay/static/media/3627634.64b04226.jpg"
+  },
+  {
+    "revision": "8261e8488a62dc1dc8d68f0c7eb20aee",
+    "url": "/github.io/caribbeanpay/static/media/Posteos WP.8261e848.png"
+  },
+  {
+    "revision": "aae904bdf123989829b9a1ce32a03040",
+    "url": "/github.io/caribbeanpay/static/media/banesco.aae904bd.svg"
+  },
+  {
+    "revision": "c14354e7c6573900bc89514c2861aaff",
+    "url": "/github.io/caribbeanpay/static/media/homeimg.c14354e7.png"
+  },
+  {
+    "revision": "10614e2365a534d3b833be3a312b7bec",
+    "url": "/github.io/caribbeanpay/static/media/image 1.10614e23.svg"
+  },
+  {
+    "revision": "f0f11dcf0d24e46193dd5c0480601935",
+    "url": "/github.io/caribbeanpay/static/media/imagearg.f0f11dcf.png"
+  },
+  {
+    "revision": "39b61dd138191c558a3038f86af27017",
+    "url": "/github.io/caribbeanpay/static/media/imagechile.39b61dd1.svg"
+  },
+  {
+    "revision": "5f571cb4e3284bb435e144bf380c1ae1",
+    "url": "/github.io/caribbeanpay/static/media/imagehome.5f571cb4.svg"
+  },
+  {
+    "revision": "659f840609b707271531f0e10f858474",
+    "url": "/github.io/caribbeanpay/static/media/mercantil.659f8406.svg"
+  },
+  {
+    "revision": "2000a2a2d3b7f294e6e24af78ddddcde",
+    "url": "/github.io/caribbeanpay/static/media/provincial.2000a2a2.svg"
+  },
+  {
+    "revision": "4413fcb46a94324c6a877a1e8b8a5e6e",
+    "url": "/github.io/caribbeanpay/static/media/venezuela.4413fcb4.svg"
+  }
+]);
